3f680e5929f2d943b112f43924ddbc615bac9dc6
Manuel Cabezas <manuelcabezas@gmail.com>
2022-12-26 01:43:16 -0400

    feat: added tasks creation, deletion and status change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,87 @@
-let tasks = []
+// Asignar id aleatorio
+const randomId = () => Math.random().toString(16).slice(10)
 
+// Arreglo de tareas inicales
+let tasks = [
+    { id: randomId(), description: "Sacar la basura", isCompleted: false },
+    { id: randomId(), description: "Comprar verduras", isCompleted: false },
+    { id: randomId(), description: "Lavar la ropa", isCompleted: false }
+]
+
+// Inicializar variables de elementos en DOM
 const tableTasks = document.querySelector("#table-tasks");
 const description = document.querySelector("#input-description");
+const taskTotal = document.querySelector("#total-task");
+const taskCompleted = document.querySelector("#completed-task");
 
+// Cambiar estado de tarea
 const toggleCompleted = (id) => {
-    const taskIndexSelected = tasks.findIndex(t => t.id === id);
+    const taskIndexSelected = tasks.findIndex(task => task.id === id);
     tasks[taskIndexSelected].isCompleted = !tasks[taskIndexSelected].isCompleted
     renderTable();
-    if (tasks[taskIndexSelected].isCompleted) {
-        const tdDescription = document.querySelector(`#td-${id}`);
-        tdDescription.style.textDecoration = "line-through"
-    }
-
+    calculateTotalTasks();
 }
 
+// Agregar una tarea
 const addTask = () => {
-    tasks.push({ id: Date.now(), description: description.value, isCompleted: false });
-    description.value = "";
-    description.focus();
+    tasks.push({
+        id: randomId(),
+        description: description.value,
+        isCompleted: false,
+    });
+
     renderTable();
+    calculateTotalTasks();
+    clearFocusDescription();
 }
 
+// Eliminar una tarea
 const deleteTask = (id) => {
-    tasks = tasks.filter(t => t.id !== id);
+    tasks = tasks.filter(task => task.id !== id);
     renderTable();
+    calculateTotalTasks();
 }
 
+// Calcular tareas totales y completadas
+const calculateTotalTasks = () => {
+    taskTotal.innerHTML = tasks.length
+    let completed = 0;
+    for (let task of tasks) {
+        if (task.isCompleted) { completed++ }
+    }
+    taskCompleted.innerHTML = completed
+}
+
+// Renderizar tabla de tareas
 const renderTable = () => {
-    tableTasks.innerHTML = `
+    let trHtml = `
     <tr>
         <th>ID</th>
         <th>Tarea</th>
-        <th>ok?</th>
-        <th>Del</th>    
+        <th>✔️</th>
+        <th>🗑️</th>    
     </tr>
     `
     for (let task of tasks) {
-        tableTasks.innerHTML += `
+        trHtml += `
         <tr>
             <td>${task.id}</td>
-            <td id="td-${task.id}">${task.description}</td>
-            <td><input onclick="toggleCompleted(${task.id})" type="checkbox" ${task.isCompleted ? 'checked' : null}></td>
-            <td><button onclick="deleteTask(${task.id})">X</button></td>
+            <td id="td-${task.id}" ${task.isCompleted && 'class="text-line"'}>${task.description}</td>
+            <td><input onclick="toggleCompleted('${task.id}')" type="checkbox" ${task.isCompleted && 'checked'}></td>
+            <td><button onclick="deleteTask('${task.id}')">✖️</button></td>
         </tr>
         `
     }
+    tableTasks.innerHTML = trHtml;
 }
 
-/*
-{ id: 1671922591400, description: "Tarea de prueba", isCompleted: true },
-{ id: 1671922626923, description: "Tarea 2", isCompleted: false }
-*/
+// Limpiar y enfocar input de ingreso
+const clearFocusDescription = ()=>{
+    description.value = "";
+    description.focus();
+}
+
+// Cargar tareas iniciales
+renderTable();
+calculateTotalTasks();
+clearFocusDescription();
